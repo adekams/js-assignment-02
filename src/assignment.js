53ff15543f167a3e6694d1a6afaaa3063a899bdb
Adenike Salau <54279390+adekams@users.noreply.github.com>
2022-07-14 11:01:30 +0000

    task completion

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,12 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    // return arrayOfNumbers;
+	let sum = 0;
+	arrayOfNumbers.map(num => sum += num)
+		return sum
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +31,14 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    // return arrayOfNumbers.length;
+	let total = 0;
+	arrayOfNumbers.map(num => {
+	if(num % 2 == 0) total+=1	
+	})
+	return total
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
